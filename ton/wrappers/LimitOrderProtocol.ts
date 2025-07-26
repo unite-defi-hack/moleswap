901cbb315de849e085f5d5e18b699c0a78450781
IlyaTsupryk <ilya.tsupryk@gmail.com>
2025-07-27 01:43:47 +0200

    Add functionality for new order creation via LOP contract

--- ton/wrappers/LimitOrderProtocol.ts
+++ ton/wrappers/LimitOrderProtocol.ts
@@ -1,14 +1,17 @@
-import {Address, beginCell, Cell, Contract, contractAddress} from "@ton/core";
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { Op } from './opcodes';
 
 export type EscrowFactoryConfig = {
     admin: Address;
-    escrowCode: Cell;
+    srcEscrowCode: Cell;
+    dstEscrowCode: Cell;
 };
 
 export function escrowFactoryConfigToCell(config: EscrowFactoryConfig): Cell {
     return beginCell()
         .storeAddress(config.admin)
-        .storeRef(config.escrowCode)
+        .storeRef(config.srcEscrowCode)
+        .storeRef(config.dstEscrowCode)
         .endCell();
 }
 
@@ -16,8 +19,7 @@ export class LimitOrderProtocol implements Contract {
     constructor(
         readonly address: Address,
         readonly init?: { code: Cell; data: Cell },
-    ) {
-    }
+    ) {}
 
     static createFromAddress(address: Address) {
         return new LimitOrderProtocol(address);
@@ -25,7 +27,75 @@ export class LimitOrderProtocol implements Contract {
 
     static createFromConfig(config: EscrowFactoryConfig, code: Cell, workchain = 0) {
         const data = escrowFactoryConfigToCell(config);
-        const init = {code, data};
+        const init = { code, data };
         return new LimitOrderProtocol(contractAddress(workchain, init), init);
+    }
+
+    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().endCell(),
+        });
+    }
+
+    async sendCreateOrder(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            value: bigint;
+            queryId: number;
+            makerAddress: Address;
+            makerAsset: Address;
+            makingAmount: bigint;
+            receiverAddress: bigint;
+            takerAsset: bigint;
+            takingAmount: bigint;
+            salt: bigint;
+            hashlock: bigint;
+            expirationTime: bigint;
+        },
+    ) {
+        return await provider.internal(via, {
+            value: opts.value + opts.makingAmount,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Op.create_order, 32)
+                .storeUint(opts.queryId, 64)
+                .storeAddress(opts.makerAddress)
+                .storeAddress(opts.makerAsset)
+                .storeUint(opts.makingAmount, 128)
+                .storeUint(opts.receiverAddress, 256)
+                .storeRef(
+                    beginCell()
+                        .storeUint(opts.takerAsset, 256)
+                        .storeUint(opts.takingAmount, 128)
+                        .storeUint(opts.salt, 128)
+                        .storeUint(opts.hashlock, 256)
+                        .storeUint(opts.expirationTime, 64)
+                        .endCell(),
+                )
+                .endCell(),
+        });
+    }
+
+    async getSrcEscrowAddress(provider: ContractProvider, orderHash: bigint) {
+        const result = await provider.get('get_src_escrow_address', [
+            {
+                type: 'int',
+                value: orderHash,
+            },
+        ]);
+        return result.stack.readAddress();
+    }
+
+    async getDstEscrowAddress(provider: ContractProvider, orderHash: bigint) {
+        const result = await provider.get('get_dst_escrow_address', [
+            {
+                type: 'int',
+                value: orderHash,
+            },
+        ]);
+        return result.stack.readAddress();
     }
 }
