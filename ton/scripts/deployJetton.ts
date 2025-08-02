3c5f0507131c099fe16ac76ffba983e2dd1c75e6
IlyaTsupryk <ilya.tsupryk@gmail.com>
2025-08-02 10:44:47 +0200

    Add scripts for jettons deployment

---
+++ ton/scripts/deployJetton.ts
@@ -0,0 +1,24 @@
+import { Address, beginCell, toNano } from '@ton/core';
+import { compile, NetworkProvider } from '@ton/blueprint';
+import { JettonMinter } from '../wrappers/JettonMinter';
+
+export async function run(provider: NetworkProvider) {
+    const ui = provider.ui();
+
+    const manifestUrl =
+        'https://raw.githubusercontent.com/refs/heads/main/unite-defi-hack/moleswap/scripts/jettons/mole.json';
+
+    const tokenMinter = provider.open(
+        JettonMinter.createFromConfig(
+            {
+                admin: provider.sender().address as Address,
+                content: beginCell().storeUint(1, 8).storeStringTail(manifestUrl).endCell(),
+                walletCode: await compile('JettonWallet'),
+            },
+            await compile('JettonMinter'),
+        ),
+    );
+    ui.write(`Deploy JettonMinter contract...`);
+    await tokenMinter.sendDeploy(provider.sender(), toNano('0.1'));
+    await provider.waitForDeploy(tokenMinter.address);
+}
